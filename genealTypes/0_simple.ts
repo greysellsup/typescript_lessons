98d9aaf600947f542e22f1d2e83c95114ac88e61
grsl <greysellsup@gmail.com>
2022-10-12 23:40:52 +0500

    add general types

---
+++ genealTypes/0_simple.ts
@@ -0,0 +1,9 @@
+
+// простые типы string, number, boolean
+const revenue: number = 1000;
+const bonus: number = 500;
+const fName: string = 'Sergey';
+const man: boolean = true;
+
+const res: number = revenue + bonus;
+console.log(res);
---
+++ genealTypes/1_function.ts
@@ -0,0 +1,9 @@
+function getFullName(fName: string, sName: string): string {
+    return `${fName} ${sName}`
+}
+
+const getFullNameArrow = (fName: string, sName: string):string => {
+    return `${fName} ${sName}`
+}
+
+console.log(getFullNameArrow('Sega', 'Mega'));
