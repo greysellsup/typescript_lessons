ce20c504a432fa7d6ba11424c14f11be5d397b4a
Сергей Чистяков <grsl@MacBook-Pro-16.local>
2023-07-16 15:13:14 +0500

    типизация объекта

--- genealTypes/1_function.ts
+++ genealTypes/1_function.ts
@@ -2,8 +2,32 @@ function getFullName(fName: string, sName: string): string {
     return `${fName} ${sName}`
 }
 
-const getFullNameArrow = (fName: string, sName: string):string => {
+const getFullNameArrow = (fName: string, sName: string): string => {
     return `${fName} ${sName}`
 }
 
 console.log(getFullNameArrow('Sega', 'Mega'));
+
+//типизация объекта
+let obj: {
+    "officeId": number,
+    "isOpened": false,
+    "contacts": {
+        "phone": string,
+        "email": string,
+        "address": {
+            "city": string
+        }
+    }
+} =
+{
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+79100000000",
+        "email": "my@email.ru",
+        "address": {
+            "city": "Москва"
+        }
+    }
+}
